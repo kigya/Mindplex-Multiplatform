0c100f2ef8ec73cb9122b4fe6cdf4ff1c50d7648
DropDrage <danilov.retar@gmail.com>
2024-05-04 23:02:11 +0300

    MPMP-5: removed detekt test code #5

--- androidApp/src/main/kotlin/dev/kigya/mindplex/androidApp/MainActivity.kt
+++ androidApp/src/main/kotlin/dev/kigya/mindplex/androidApp/MainActivity.kt
@@ -11,15 +11,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import dev.kigya.mindplex.Greeting
-import org.w3c.dom.Text
-
-const val A_A = 1
-val aA = Regex("2")
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        SomeFunction()
         setContent {
             MyApplicationTheme {
                 Surface(
@@ -44,10 +39,4 @@ private fun DefaultPreview() {
     MyApplicationTheme {
         GreetingView("Hello, Android!")
     }
-}
-
-private const val CONST = 4
-
-private fun SomeFunction(): Int {
-    return CONST
 }
