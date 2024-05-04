22ba9610da75f08fd658398b612bf4e462b34a3a
DropDrage <danilov.retar@gmail.com>
2024-05-04 21:02:47 +0300

    MPMP-5: detekt CI rework #5

--- build-logic/gradle-ext/src/main/kotlin/detekt/DetektConfigs.kt
+++ build-logic/gradle-ext/src/main/kotlin/detekt/DetektConfigs.kt
@@ -1,7 +1,7 @@
 package detekt
 
 object DetektConfigs {
-    private const val DETEKT_CONFIG_PATH = "detekt"
+    private const val DETEKT_CONFIG_PATH = "config/detekt"
 
     const val MAIN = "$DETEKT_CONFIG_PATH/main.yml"
     const val COMPOSE = "$DETEKT_CONFIG_PATH/compose.yml"
