d29e9c665cab45fefc5e3575f7d124fe23dac444
kigya <kirillborichevskiy@gmail.com>
2025-03-25 15:15:24 +0300

    MPMP-35: Migrated to Google LocalActivity instead of custom one

--- core/presentation/common/build.gradle.kts
+++ core/presentation/common/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.kotlin.dsl.dependencies
+
 plugins {
     with(libs.plugins.convention) {
         alias(config.shared.library)
@@ -8,6 +10,11 @@ plugins {
 
 kotlin {
     sourceSets {
+        androidMain.dependencies {
+            with(libs) {
+                implementation(compose.android.activity)
+            }
+        }
         commonMain.dependencies {
             with(libs) {
                 implementation(compose.navigation)
