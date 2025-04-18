d4a06168e814de6ccef5192b5fb97a8cc8154c9d
kigya <kirillborichevskiy@gmail.com>
2025-04-18 15:38:20 +0300

    MPMP-56: Added key removal while signing out

--- feature/login/data/src/commonMain/kotlin/dev.kigya.mindplex.feature.login.data/repository/repository/SignInPreferencesRepository.kt
+++ feature/login/data/src/commonMain/kotlin/dev.kigya.mindplex.feature.login.data/repository/repository/SignInPreferencesRepository.kt
@@ -4,7 +4,6 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
-import dev.kigya.mindplex.core.util.extension.empty
 import dev.kigya.mindplex.feature.login.domain.contract.SignInPreferencesRepositoryContract
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
@@ -15,22 +14,18 @@ class SignInPreferencesRepository(
     private val dataStore: DataStore<Preferences>,
     private val dispatcher: CoroutineDispatcher,
 ) : SignInPreferencesRepositoryContract {
-
     override val userToken: Flow<String?>
         get() = dataStore.data.map { preferences ->
             preferences[stringPreferencesKey(GOOGLE_ID_TOKEN)]
         }
 
     override val isSignedIn: Flow<Boolean>
-        get() = dataStore.data.map { preferences ->
-            preferences[stringPreferencesKey(IS_SIGNED_IN)]?.toBoolean() ?: false
-        }
+        get() = userToken.map { !it.isNullOrBlank() }
 
     override suspend fun signIn(googleIdToken: String) {
         withContext(dispatcher) {
             dataStore.edit { preferences ->
                 preferences[stringPreferencesKey(GOOGLE_ID_TOKEN)] = googleIdToken
-                preferences[stringPreferencesKey(IS_SIGNED_IN)] = "true"
             }
         }
     }
@@ -38,14 +33,12 @@ class SignInPreferencesRepository(
     override suspend fun signOut() {
         withContext(dispatcher) {
             dataStore.edit { preferences ->
-                preferences[stringPreferencesKey(GOOGLE_ID_TOKEN)] = String.empty
-                preferences[stringPreferencesKey(IS_SIGNED_IN)] = "false"
+                preferences.remove(stringPreferencesKey(GOOGLE_ID_TOKEN))
             }
         }
     }
 
     private companion object {
         const val GOOGLE_ID_TOKEN = "google_id_token"
-        const val IS_SIGNED_IN = "is_signed_in"
     }
 }
